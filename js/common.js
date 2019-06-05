4e92e3ed649b5e78efb80ad72c1feed656200cf8
Amoral1s <43203197+Amoral1s@users.noreply.github.com>
2019-06-05 19:41:04 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  
+  $(function () {
+    $(document).on('mousemove', function (e) {
+        $('.header__wrap').css({
+            left: -e.pageX / 20 + 600,
+            top: -e.pageY / 20 + 250
+        });
+        
+    });
+});
+
+$(".toggle, .close").click(function() {
+	if ($(".mobile").is(":visible")) {
+        $(".mobile").slideUp(600);
+        
+	} else {
+		$(".mobile").slideDown(600);
+	};
+});
+
+
+	$(".toggle").click(function() {
+        $(".toggle").addClass('open-toggle');
+        $(".close").addClass('open-x');
+       });
+	$(".close").click(function() {
+        $(".toggle").removeClass('open-toggle');
+        $(".close").removeClass('open-x');
+       });
+
+});
